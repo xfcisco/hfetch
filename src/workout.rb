6026faaa62aee56b8380627cf3a1796d600aab8b
xfcisco <xfc1@pm.me>
2023-12-21 17:00:10 +0200

    set up local workout db + analyze command boilerplate

--- src/workout.rb
+++ src/workout.rb
@@ -3,7 +3,7 @@ require 'json'
 def handle_workout(s_workout)
   File.open("/home/#{ENV['USER']}/.config/hfetch.json", "r+") do |f|
     db_json = JSON.parse(f.read);
-    db_json["#{Time.new.strftime("%Y-%m-%d %H:%M:%S")}"] = JSON.parse(s_workout);
+    db_json["#{Time.new.strftime("%Y-%m-%d %H:%M")}"] = JSON.parse(s_workout);
 
     f.rewind;
     f.write (JSON.generate db_json);
