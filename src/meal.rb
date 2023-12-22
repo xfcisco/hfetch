879887b85a007c7034afee7b320a86e41d4bbf9f
xfcisco <xfc1@pm.me>
2023-12-22 11:51:31 +0200

    implemented +meal command

--- src/meal.rb
+++ src/meal.rb
@@ -1,5 +1,11 @@
 require 'json'
 
 def handle_meal(s_meal)
-end
+  File.open("/home/#{ENV['USER']}/.config/hfetch.meals.json", "r+") do |f|
+    db_json = JSON.parse(f.read);
+    db_json["#{Time.new.strftime("%Y-%m-%d %H:%M")}"] = JSON.parse(s_meal);
 
+    f.rewind;
+    f.write (JSON.generate db_json);
+  end
+end
